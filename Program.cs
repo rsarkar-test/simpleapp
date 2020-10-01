7bea4ba09f428d95362d1b1e3d00e7846def6183
rsarkar-test <53563278+rsarkar-test@users.noreply.github.com>
2020-10-01 17:22:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+namespace NetCore.Docker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		    while(true)
+			{
+            Console.WriteLine("Welcome to Devops !!");
+			Thread.Sleep(2000);
+			}
+        }
+    }
+}
